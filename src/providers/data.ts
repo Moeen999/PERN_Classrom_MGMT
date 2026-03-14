4c71c6c58d31f70faa0d42caffd07c82b35b2e56
Muhammad Moeen <mooenmalik374@gmail.com>
2026-03-14 06:13:18 +0500

    implemented the mock data providers feature.

--- src/providers/data.ts
+++ src/providers/data.ts
@@ -1,45 +1,5 @@
-import {
-    BaseRecord,
-    DataProvider,
-    GetListParams,
-    GetListResponse,
-} from "@refinedev/core";
-import {mockSubjects} from "@/constants/mock-subjects.ts";
-
-export const dataProvider: DataProvider = {
-    getList: async <
-        TData extends BaseRecord = BaseRecord
-    >(
-        { resource }: GetListParams
-    ): Promise<GetListResponse<TData>> => {
-        if (resource !== "subjects") {
-            return {
-                data: [] as TData[],
-                total: 0,
-            };
-        }
-
-        return {
-            data: mockSubjects as unknown as TData[],
-            total: mockSubjects.length,
-        };
-    },
-
-    getOne: async () => {
-        throw new Error("This func is not in mock data providers");
-    },
-
-    create: async () => {
-        throw new Error("This func is not in mock data providers");
-    },
-
-    update: async () => {
-        throw new Error("This func is not in mock data providers");
-    },
-
-    deleteOne: async () => {
-        throw new Error("This func is not in mock data providers");
-    },
-
-    getApiUrl: () => "",
-};
+import { createSimpleRestDataProvider } from "@refinedev/rest/simple-rest";
+import { API_URL } from "./constants";
+export const { dataProvider, kyInstance } = createSimpleRestDataProvider({
+    apiURL: API_URL,
+});
